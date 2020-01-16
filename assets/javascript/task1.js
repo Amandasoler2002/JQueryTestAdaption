c616cb269804f44ec65083d77b589acb72d96362
Soler <20699@vtsd.com>
2020-01-16 11:14:06 -0500

    jQuery Test Completed

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,1 +1,9 @@
 /* Put code for the task below */
+$(document).ready(function(){
+  $("#imageTask1").hover(function(){
+    $("#buttonTask1").show();
+  });
+  $("#buttonTask1").click(function(){
+    $("#imageTask1").hide();
+  });
+});
