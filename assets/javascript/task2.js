b2c6ad719cf4360e7678caec8ad60a8837502be7
Hailey Ipsale <hailey.ipsale@vtsd.com>
2020-01-15 20:27:37 -0500

    All Tasks Completed

--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,6 +1,12 @@
 /* Put code for the task below */
 $(document).ready(function(){
   $("#color1").click(function(){
-
+    $("#tableNames").css("background-color", "orange");
+  });
+  $("#color2").click(function(){
+    $("#tableNames").css("background-color", "pink");
+  });
+  $("#color3").click(function(){
+    $("#tableNames").css("background-color", "yellow");
   });
 });
